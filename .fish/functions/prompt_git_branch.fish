f2be088ee761044abf69b709867cafdf8075105d
Jason Dsouza <jdsouza@tripadvisor.com>
2013-04-22 18:49:12 -0400

    Updated irbrc to use pp instead of ap

--- .fish/functions/prompt_git_branch.fish
+++ .fish/functions/prompt_git_branch.fish
@@ -3,8 +3,9 @@
 # Mainly for use in the command prompt
 #
 
+set fish_git_branch_color purple
+
 function prompt_git_branch --description "Get the current git branch"
-  set fish_git_branch_color purple
   set -l branch (git symbolic-ref HEAD 2> /dev/null | cut -d/ -f3-)
   if test -n "$branch"
     printf "%s[%s] " (set_color $fish_git_branch_color) $branch
