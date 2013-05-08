d043c8666a9abb833bc427da0c2a4ca2afc7eb2f
Jason Dsouza <jdsouza@tripadvisor.com>
2013-05-08 19:38:30 -0400

    Updated config

--- .fish/config.fish
+++ .fish/config.fish
@@ -11,16 +11,34 @@ rvm > /dev/null
 
 #
 # Add custom locations to $PATH
+# - golang bin
+# - my gocode bin
+# - dart bin
+# - local bin
 # 
-set -U fish_user_paths /usr/local/go/bin $HOME/gocode/bin /usr/local/dart/dart-sdk/bin 
+set -g fish_user_paths /usr/local/go/bin $HOME/gocode/bin /usr/local/dart/dart-sdk/bin {$HOME}/bin
 
 #
 # Golang environment variables
 #
-set -U -x GOPATH {$HOME}/gocode
+set -g -x GOPATH {$HOME}/gocode
 
 #
 # Set EDITOR variable (not sure if its needed)
 #
 set -g -x EDITOR vim
+
+#
+# Add personal functions path
+#
+#set -g fish_function_path $fish_function_path {$HOME}/<personal_functions_dir>
+
+#
+# Source TA config file
+#
+set -g -x TRIP_SCRIPTS {$HOME}/tr-users/jdsouza
+if test -f {$TRIP_SCRIPTS}/fish/trip_config.fish
+  . {$TRIP_SCRIPTS}/fish/trip_config.fish
+end
+
 
