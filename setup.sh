6577efd237a96c675f5950e84b035976b430ed35
jdsouza <jdsouza@tripadvisor.com>
2013-09-03 11:23:03 -0400

    Remove .vim dir from setup, and get rid of submodules

--- setup.sh
+++ setup.sh
@@ -19,10 +19,6 @@ ln -s ~/dotfiles/.bashrc ~/.bashrc
 # Vim
 rm ~/.vimrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-rm -rf ~/.vim
-ln -s ~/dotfiles/.vim ~/.vim
-# Recheck out the submodule git repos
-git submodule update --init
 
 # Pry
 rm ~/.pryrc
