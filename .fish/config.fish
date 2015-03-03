c71bd1fc3fb6679dae2ad656b802e0aec9a01c80
Jason Dsouza <jasonrdsouza@gmail.com>
2015-03-03 17:00:19 -0500

    Add Localytics config, and change . to the more explicit "source"

--- .fish/config.fish
+++ .fish/config.fish
@@ -30,7 +30,14 @@ set -g -x EDITOR "vim --noplugin"
 #
 set -g -x TRIP_SCRIPTS {$HOME}/tr-users/jdsouza
 if test -f {$TRIP_SCRIPTS}/fish/trip_config.fish
-  . {$TRIP_SCRIPTS}/fish/trip_config.fish
+  source {$TRIP_SCRIPTS}/fish/trip_config.fish
 end
 
+#
+# Source Localytics config file
+#
+set -g -x LOCALYTICS_CONFIG {$HOME}/.localytics_config.fish
+if test -f $LOCALYTICS_CONFIG
+  source $LOCALYTICS_CONFIG
+end
 
