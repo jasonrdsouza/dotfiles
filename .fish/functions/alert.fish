0cc0e009234932c956d521f2de6f9909e37bbab8
Jason Dsouza <jasonrdsouza@gmail.com>
2015-12-08 18:44:18 -0500

    Update alert utility to use hammerspoon

--- .fish/functions/alert.fish
+++ .fish/functions/alert.fish
@@ -1,9 +1,13 @@
 #
 # Helper function to alert the user when a command has finished running
 # Especially useful for long-running commands.
-# Uses the notify-send utility
+# Uses Hammerspoon, and depends on the following URLevent configuration:
+#
+# hs.urlevent.bind("alert", function(eventName, params)
+#     hs.alert.show(params["value"])
+# end)
 #
 
 function alert --description "Popup alert when something finishes running"
-  command notify-send --urgency=low "$argv"
+  open -g "hammerspoon://alert?value=$argv"
 end
