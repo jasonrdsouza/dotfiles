915d60f9cb349ec4842d6db4181224b05608c436
Jason Dsouza <jasonrdsouza@gmail.com>
2023-05-26 17:38:20 -0400

    Add vault helper

---
+++ .fish/functions/vault.fish
@@ -0,0 +1,16 @@
+#
+# Helper functionality to deal with my custom vault
+#
+
+function vault --description "Manipulate vault files"
+  switch $argv[1]
+  case 'unlock'
+    age --decrypt $argv[2] | tar --extract --gzip -
+  case 'lock'
+    set baseName (string trim --chars='/' $argv[2])
+    tar --create --compress $argv[2] | age --encrypt --passphrase > $baseName.tar.gz.age
+  case '*'
+    echo "Unknown command"
+  end
+end
+
